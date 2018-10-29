6d98f7cbbc5728c312913397406fd37ba4e1b722
jchamberlain909 <jchamberlain909@gmail.com>
2018-10-28 21:17:05 -0400

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_reader :number
+  
+    def initialize(num)
+      @number = largest_prime_factor(num)
+    end
+  
+    def largest_prime_factor(num)
+      prime = num
+      (2..Math.sqrt(num).to_i).each do |i|
+        break if prime <= 1
+        prime /= i while (prime > i && prime % i == 0)
+      end
+      prime
+    end
+  end
